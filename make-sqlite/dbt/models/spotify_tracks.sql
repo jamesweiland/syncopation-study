5d4710145b8654be195ad8b97386a2e629916132
jamesweiland <jameskw2@illinois.edu>
2025-01-28 22:09:37 -0600

    add fixed synpy; create midi_files.sql, spotify_tracks.sql, midi_spotify_map.sql, and db schema

---
+++ make-sqlite/dbt/models/spotify_tracks.sql
@@ -0,0 +1,18 @@
+config{{(
+    materialized="create",
+    post_hook="create index idx_sid on {{ this.name }} (spotify_id)"
+  )
+}}
+
+create table {{ this }} (
+    spotify_id varchar primary key,
+    artists varchar not null,
+    album varchar not null,
+    genres varchar not null,
+    year_first_released integer not null,
+    duration integer not null,
+    danceability float,
+    acousticness float,
+    energy float,
+    valence float,
+)
